0b6f5f594c4f19c57233de62ec1714564ee7c6f6
Glenn Mendonca <glenm.code@gmail.com>
2022-11-18 11:57:11 +0530

    Firebase hosting test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import reactLogo from "./assets/react.svg";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      Hello World <br />
+      This is glenn <br />
+      ._. Github Test
+    </div>
+  );
 }
 
 export default App;
